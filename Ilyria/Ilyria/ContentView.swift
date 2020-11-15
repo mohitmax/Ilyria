6b2dd006a482928bc249d86a16d1b6c618265353
Mohit Sadhu <mohitmax@gmail.com>
2020-11-15 15:44:07 -0500

    create project commit

---
+++ Ilyria/Ilyria/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ilyria
+//
+//  Created by mickey on 2020-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ilyria/Ilyria/IlyriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IlyriaApp.swift
+//  Ilyria
+//
+//  Created by mickey on 2020-11-15.
+//
+
+import SwiftUI
+
+@main
+struct IlyriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
