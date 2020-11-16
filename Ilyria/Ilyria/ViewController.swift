b52e53f5b174cf40d3629355be6e148a74dced91
Mohit Sadhu <mohitmax@gmail.com>
2020-11-15 23:17:59 -0500

    uikit proj

---
+++ Ilyria/Ilyria/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Ilyria
+//
+//  Created by mickey on 2020-11-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var heroAttackButton: UIButton!
+    @IBOutlet weak var monsterAttackButton: UIButton!
+    @IBOutlet weak var combatLogLabel: UILabel!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
