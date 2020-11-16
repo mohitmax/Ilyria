0dce59e198cd691bee0abde6f9384a2f8d4851cf
Mohit Sadhu <mohitmax@gmail.com>
2020-11-16 16:45:07 -0500

    added base create hero controller

---
+++ Ilyria/Ilyria/CreateCreature/CreateHeroViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateHeroViewController.swift
+//  Ilyria
+//
+//  Created by mickey on 2020-11-16.
+//
+
+import UIKit
+
+class CreateHeroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Ilyria/Ilyria/ViewController.swift
+++ Ilyria/Ilyria/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.title = "Ilyria RPG"
+        
         createCreatures()
     }
 
@@ -71,6 +73,10 @@ class ViewController: UIViewController {
             
             updateUI()
         }
+    }
+    
+    @IBAction func createHeroAction(_ sender: UIButton) {
+        
     }
     
     func updateCombatLogs() {
